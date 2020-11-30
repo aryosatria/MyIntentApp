39989014d7773718f51df88ebfd7c2155aef9ab8
satria <aryozatria04@gmail.cpm>
2020-11-30 18:42:45 +0700

    How to make IntentApp

---
+++ app/src/main/java/com/example/myintentapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnMoveActivity = findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
+        btnMoveWithDataActivity.setOnClickListener(this);
+
+        Button btnDialPhone =findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_activity_data:
+                Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Aryo Wahyu Satria");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE,24);
+                startActivity(moveWithDataIntent);
+                break;
+
+                case R.id.btn_dial_number:
+             String phonenumber = "081281830369";
+             Intent dialPhoneIntent =new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phonenumber));
+             startActivity(dialPhoneIntent);
+             break;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/myintentapp/MoveActivity.java
@@ -0,0 +1,17 @@
+package com.example.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MoveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+    }
+}
